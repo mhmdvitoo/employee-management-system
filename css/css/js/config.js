7d298c089eaeb843b294c86bf58afecf843320c7
mhmdvitoo <mhmdvitoo@gmail.com>
2026-09-17 11:54:19 +0200

    Update config.js

--- css/css/js/config.js
+++ css/css/js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL =
-    "PUT_YOUR_SUPABASE_URL_HERE";
+    "https://axnlxpovmzywxxevbxzl.supabase.co/rest/v1/";
 
 const SUPABASE_ANON_KEY =
     "PUT_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
